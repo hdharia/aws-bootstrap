746aef51e8799b39c4e1abcc9c2758821ed57f4c
hdharia <hdharia@cloudbees.com>
2016-01-29 16:06:58 -0500

    Update jenkins-slave-ami-java-bootstrap.sh

--- jenkins-slave-ami-java-bootstrap.sh
+++ jenkins-slave-ami-java-bootstrap.sh
@@ -3,13 +3,18 @@
 #install all packages
 sudo yum -y update
 sudo yum -y install git
-sudo yum -y install apache-maven
+
+
+sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+sudo yum install -y apache-maven
+mvn --version
 
 #install java jdk
-sudo wget --no-cookies --no-check-certificate --header -O /home/ec2-user/jdk-8u60-linux-x64.rpm "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jdk-8u60-linux-x64.rpm"
-sudo yum localinstall /home/ec2-user/jdk-8u60-linux-x64.rpm
+sudo wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jdk-8u60-linux-x64.rpm"
+sudo yum -y localinstall jdk-8u60-linux-x64.rpm
 
-sudo rm /home/ec2-user/jdk-8u60-linux-x64.rpm
+sudo rm ~/jdk-8u60-linux-x64.rpm
 
 #download slave.jar file
 sudo wget -O /home/ec2-user/slave.jar https://github.com/hdharia/aws-bootstrap/blob/master/slave.jar?raw=true
