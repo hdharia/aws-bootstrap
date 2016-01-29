63ed66f318e922b6da2c0f3c2d11ee42588ecc68
hdharia <hdharia@cloudbees.com>
2016-01-29 15:46:59 -0500

    Update jenkins-slave-ami-java-bootstrap.sh

--- jenkins-slave-ami-java-bootstrap.sh
+++ jenkins-slave-ami-java-bootstrap.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 #install all packages
-yum -y update
-yum -y install git
-yum -y install apache-maven
+sudo yum -y update
+sudo yum -y install git
+sudo yum -y install apache-maven
+
+#install java jdk
+sudo wget --no-cookies --no-check-certificate --header -O /home/ec2-user/jdk-8u60-linux-x64.rpm "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jdk-8u60-linux-x64.rpm"
+sudo yum localinstall /home/ec2-user/jdk-8u60-linux-x64.rpm
+
+sudo rm /home/ec2-user/jdk-8u60-linux-x64.rpm
 
 #download slave.jar file
-wget -O /home/ec2-user/slave.jar https://github.com/hdharia/aws-bootstrap/blob/master/slave.jar?raw=true
+sudo wget -O /home/ec2-user/slave.jar https://github.com/hdharia/aws-bootstrap/blob/master/slave.jar?raw=true
