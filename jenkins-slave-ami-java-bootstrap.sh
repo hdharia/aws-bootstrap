18144a7a003bae2f936a9bfd4dce45d64809b4d1
hdharia <hdharia@cloudbees.com>
2016-01-29 10:46:40 -0500

    Update jenkins-slave-ami-java-bootstrap.sh

--- jenkins-slave-ami-java-bootstrap.sh
+++ jenkins-slave-ami-java-bootstrap.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #install all packages
-sudo yum -y update
-sudo yum -y install git
-sudo yum -y install apache-maven
+yum -y update
+yum -y install git
+yum -y install apache-maven
 
 #download slave.jar file
-wget -O slave.jar https://github.com/hdharia/aws-bootstrap/blob/master/slave.jar?raw=true
+wget -O /home/ec2-user/slave.jar https://github.com/hdharia/aws-bootstrap/blob/master/slave.jar?raw=true
